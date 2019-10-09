5cc6ed867c50d2fbc2a7f67386df885f3051e691
Mohidul Haque Arif <32663801+arifbd2221@users.noreply.github.com>
2019-10-09 22:41:13 +0600

    Update settings.py

--- medium/settings.py
+++ medium/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'medium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
